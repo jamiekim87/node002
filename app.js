50fd602dbdb543ab017765403cda2a344892a7bd
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-08-11 09:42:55 -0700

    push to repo

---
+++ app.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+
+//  fs.readFile('example.txt', 'utf8', (err, date) => {
+//      if (err) { console.log(err) }
+//      console.log(data)
+//  })
+
+// fs.writeFile('something.txt', 'Hi', (err) => {
+//     if (err) { console.log(err) }
+//     console.log('File Created!')
+// })
+
+//  fs.appendFile('example.txt', 'Goodbye World!', (err) => {
+//      if (err) { console.log(err) }
+//      console.log('File Updated!')
+//  })
+
+//  fs.appendFile('movies.txt', 'Movie1, Movie2, Movie3', (err) => {
+//      if (err) { console.log(err) }
+//      console.log('File Updated!') o
+//  })
+
+//  let Movie1 = process.argv[2]
+//  let Movie2 = process.argv[3]
+//  let Movie3 = process.argv[4]
+
+//  console.log(`Their name is $(Movie1) $(Movie2) and their favorite movie is $(movie}`)
+
+
+
+
+ 
